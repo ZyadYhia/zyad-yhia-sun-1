9614502a84fb776f3b990306db9807cf38782bb0
ZyadYhia <zeyad.yhia95@gmail.com>
2021-02-22 03:32:58 +0200

    solution 4

--- 4.php
+++ 4.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <!-- Bootstrap CDN  -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 
@@ -11,14 +12,26 @@
     <table style=" margin-top: 10px;width: 95%;" class="table table-dark text-center">
         <thead>
             <tr>
+                <!-- Table title -->
                 <th colspan="12">
                     Multiplication Table
                 </th>
             </tr>
         </thead>
         <tbody>
+
+
+            <!-- Nested loops to fill the equation parameters -->
+            <!-- first loop for displaying and aobtaining the first number 
+                 responsible of displaying the rows -->
+
             <?php for ($i = 1; $i <= 12; $i++) { ?>
                 <tr>
+
+                    <!-- second loop for displaying and aobtaining the second number 
+                         and the result of multiplication equation
+                         responsible of displaying the coloums of each row -->
+
                     <?php for ($y = 1; $y <= 12; $y++) {
                         $z = $y * $i; ?>
                         <td class="text-center border border-3">
